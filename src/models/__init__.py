2bafff7fce1c3f05e99d0dba3ab2142335e8cb62
OptimusRahul <rockerahul97@gmail.com>
2025-03-31 15:53:07 +0530

    feat(billing): changed billing to wallet based system and deduct usage based system

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,7 @@
-from src.database import Base
+# from src.database import Base
+
 # from src.services.auth.model import UserModel
 # from src.services.teams.model import TeamMemberModel, TeamModel
+from .billing_models import BillingPlanModel, ChargeModel, TransactionModel, TransactionStatus, TransactionType, WalletModel
 
-__all__ = ["Base"]
+__all__ = ["TransactionModel", "WalletModel", "ChargeModel", "BillingPlanModel", "TransactionStatus", "TransactionType"]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,8 @@
 # ruff: noqa: F401
 from .__load_modules import ModuleLoader
 from .auth_util import create_access_token, get_password_hash, verify_password
+from .charge import Charge
 from .email import EmailUtil
 from .s3 import generate_unique_filename
 
-__all__ = ["ModuleLoader", "EmailUtil", "create_access_token", "get_password_hash", "verify_password", "generate_unique_filename"]
+__all__ = ["ModuleLoader", "EmailUtil", "create_access_token", "get_password_hash", "verify_password", "generate_unique_filename", "Charge"]
